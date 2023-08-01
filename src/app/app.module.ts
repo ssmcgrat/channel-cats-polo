ec25e77c4d03793679d3a25c4fd340fb57f11261
Sean McGrath <ssmcgrat@gmail.com>
2023-08-01 00:12:52 -0500

    basic angular template, two routes and material button configured

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { ContactComponent } from './contact/contact.component';
+import {MatButtonModule} from '@angular/material/button';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactComponent,
+    HomeComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatButtonModule,
+    RouterModule.forRoot([
+      {path: 'contact', component: ContactComponent},
+      {path: '', component: HomeComponent},
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
